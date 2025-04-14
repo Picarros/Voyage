785a19a3df745de07fa1b4ffd9a85872f01f9368
Yann GROS <yann.gros@capgemini.com>
2025-04-15 00:13:08 +0200

    Remise des notes avant les attentions

--- src/lib/components/Note.svelte
+++ src/lib/components/Note.svelte
@@ -3,15 +3,15 @@
     export let danger;
 </script>
 
+{#if note}
+<div class="d-flex align-items-center gap-2">
+    <span class="badge bg-secondary">Note</span>
+    <span style="font-size: 0.8rem;" class="text-secondary">{@html note}</span>
+</div>
+{/if}
 {#if danger}
 <div class="d-flex align-items-center gap-2">
     <span class="badge bg-danger">Attention</span>
     <span style="font-size: 0.8rem;" class="text-danger">{@html danger}</span>
-</div>
-{/if}
-{#if note}
-<div class="d-flex align-items-center gap-2">
-	<span class="badge bg-secondary">Note</span>
-	<span style="font-size: 0.8rem;" class="text-secondary">{@html note}</span>
 </div>
 {/if}
