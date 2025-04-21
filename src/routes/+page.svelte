2a0f862a53498e5793c12d7bccd1c9d01f4d09c2
Yann GROS <yann.gros@capgemini.com>
2025-04-21 18:48:51 +0200

    Ajout du fold / unfold all

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,7 @@
 
 	let data;
 	let tripId = '';
+	let allOpen = true;
 
 	onMount(async () => {
 		const response = await fetch(`${base}/data/data.json`);
@@ -19,21 +20,64 @@
 		}
 	});
 
+	function toggleAll() {
+		allOpen = !allOpen;
+
+		if (allOpen) {
+			document.querySelectorAll('.accordion-button.collapsed').forEach((ac) => {
+				ac.click();
+			});
+		} else {
+			document.querySelectorAll('.accordion-button:not(.collapsed)').forEach((ac) => {
+				ac.click();
+			});
+		}
+	}
+
 	$: if (data && tripId) {
 		trip.set(data?.trips.find((v) => v.id === tripId));
 	}
 </script>
 
-<div class="container">
-	<!-- Sélection du voyage -->
-	<div class="row mb-5">
-		<select class="form-select" bind:value={tripId}>
-			{#each data?.trips ?? [] as v}
-				<option value={v.id}>{v.name}</option>
-			{/each}
-		</select>
+<!-- Navbar -->
+<nav class="navbar navbar-dark bg-dark sticky-top">
+	<div class="container">
+		<div class="d-flex w-100">
+			<!-- Sélection du voyage -->
+			<div class="w-100 pe-2">
+				<select class="form-select" bind:value={tripId}>
+					{#each data?.trips ?? [] as v}
+						<option value={v.id}>{v.name}</option>
+					{/each}
+				</select>
+			</div>
+			<!-- Fold / Unfold -->
+			<div class="flex-shrink-1">
+				<button class="btn btn-primary" on:click={toggleAll}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						height="20px"
+						viewBox="0 -960 960 960"
+						width="20px"
+						fill="#FFFFFF"
+					>
+						{#if allOpen}
+							<path
+								d="m369-192-51-51 162-162 162 162-51 51-111-111-111 111Zm111-363L318-717l51-51 111 111 111-111 51 51-162 162Z"
+							/>
+						{:else}
+							<path
+								d="M480-144 318-306l51-51 111 111 111-111 51 51-162 162ZM369-603l-51-51 162-162 162 162-51 51-111-111-111 111Z"
+							/>
+						{/if}
+					</svg>
+				</button>
+			</div>
+		</div>
 	</div>
+</nav>
 
+<div class="container">
 	<!-- Affichage du voyage -->
 	{#if $trip}
 		{#each $trip.days as day, dayIndex}
@@ -50,7 +94,7 @@
 								aria-expanded="true"
 								aria-controls="collapse{dayIndex}"
 							>
-								<DayHeader day={day} />
+								<DayHeader {day} />
 							</button>
 						</h2>
 						<!-- Contenu -->
@@ -61,7 +105,7 @@
 							data-bs-parent="#accordion{dayIndex}"
 						>
 							<div class="accordion-body p-0">
-								<Day day={day} />
+								<Day {day} />
 							</div>
 						</div>
 					</div>
